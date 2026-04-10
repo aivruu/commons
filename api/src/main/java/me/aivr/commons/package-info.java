173ec7372c340b40293d407a174d3d1f0cdab0fb
aivruu <benjaalejandro206@gmail.com>
2026-04-10 14:43:43 -0400

    docs: write documentation for `me.aivr.commons` package on `api` subproject

---
+++ api/src/main/java/me/aivr/commons/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Provides API-related classes to provide API instances and handle access to different API components.
+ *
+ * @since 1.0.0
+ */
+@NullMarked
+package me.aivr.commons;
+
+import org.jspecify.annotations.NullMarked;
