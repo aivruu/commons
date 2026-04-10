37dbd4c0252a3c276f39ab0688bb3a0c7176daa3
aivruu <benjaalejandro206@gmail.com>
2026-04-10 15:34:24 -0400

    docs(util): move utilities-package documentation

--- api/src/main/java/me/aivr/commons/util/application/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Provides application utility-related classes.
- *
- * @since 1.0.0
- */
-@NullMarked
-package me.aivr.commons.util.application;
-
-import org.jspecify.annotations.NullMarked;
---
+++ api/src/main/java/me/aivr/commons/util/domain/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Provides domain utility-related classes.
+ *
+ * @since 1.0.0
+ */
+@NullMarked
+package me.aivr.commons.util.domain;
+
+import org.jspecify.annotations.NullMarked;
