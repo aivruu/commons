0e63fad00da4b73af12278e938666280d5fcaf92
aivruu <benjaalejandro206@gmail.com>
2026-05-18 13:49:00 -0400

    build(core): make dependencies used available for external consumers

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,10 +1,16 @@
 plugins {
   id("commons.publish-conventions")
+//  id("commons.test-conventions")
 }
 
 dependencies {
   api(project(":${rootProject.name}-api"))
 
   compileOnlyApi(libs.paper.api)
-  compileOnly(libs.configurate)
+  compileOnlyApi(libs.configurate)
+//  compileOnlyApi(libs.configurate.yaml)
+//  compileOnlyApi(libs.configurate.json)
+
+//  testRuntimeOnly(libs.configurate.yaml)
+//  testRuntimeOnly(libs.configurate.json)
 }
