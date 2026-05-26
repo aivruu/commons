5d9919555bc1a7dfb8950bd666fbb7af87234fe6
aivruu <benjaalejandro206@gmail.com>
2026-05-26 09:12:30 -0400

    feat(infra/AbstractInMemoryLocalRegistry): provide abstraction for common-logic between registries

---
+++ infrastructure/local/src/main/java/me/aivr/commons/registry/infrastructure/AbstractInMemoryLocalRegistry.java
@@ -0,0 +1,106 @@
+// This file is part of "commons", licensed under the GNU License.
+//
+// Copyright (c) 2026 aivruu
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+package me.aivr.commons.registry.infrastructure;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import me.aivr.commons.registry.domain.LocalRegistry;
+import org.jspecify.annotations.NullMarked;
+
+/**
+ * A class that abstracts common-logic related to registries and serves for registry-implementations that handle expirable and
+ * non-expirable entries.
+ *
+ * @param <K> the type of id this registry uses.
+ * @param <V> the type of value this registry handles.
+ * @param <M> the map-implementation this registry uses.
+ * @since 2.3.0
+ */
+@NullMarked
+public abstract class AbstractInMemoryLocalRegistry<K, V, M extends Map<K, V>> implements LocalRegistry<K, V> {
+  /**
+   * The {@link Map} type to use as cache-handler for this registry.
+   *
+   * @since 2.3.0
+   */
+  protected final M cache;
+
+  protected AbstractInMemoryLocalRegistry(final M cache) {
+    this.cache = cache;
+  }
+
+  /**
+   * {@inheritDoc}
+   * <p>
+   * This function will create a new modifiable-collection for all this registry's values with a size resolvable as this registry's
+   * count of stored-entries as of now, also known as -> {@code this.cache.values().size()}.
+   * <p>
+   * Modifications performed over the returned collection won't reflect any changes on the original collection.
+   *
+   * @since 2.3.0
+   */
+  @Override
+  @SuppressWarnings("unchecked")
+  public final <C extends Collection<V>> C findAll(final Consumer<V> postFetchAction) {
+    final Collection<V> registryValues = this.cache.values();
+    final Collection<V> values = new ArrayList<>(registryValues.size());
+    values.addAll(registryValues);
+    return (C) values;
+  }
+
+  /**
+   * {@inheritDoc}
+   * <p>
+   * This function will create a new modifiable-collection for the filtered-values with a size resolvable as this registry's
+   * count of stored-entries as of now, also known as -> {@code this.cache.values().size()}.
+   * <p>
+   * Elements will be filtered from the original collection then stored into the new one based on elements' (object) properties,
+   * depending on how the {@code condition} parameter was configured.
+   * <p>
+   * Once the filtering finished then the new collection will be returned by this function. Modifications performed over the
+   * returned collection won't reflect any changes on the original collection.
+   *
+   * @since 2.3.0
+   */
+  @Override
+  @SuppressWarnings("unchecked")
+  public final <C extends Collection<V>> C filter(final Predicate<V> condition) {
+    final Collection<V> registryValues = this.cache.values();
+    final Collection<V> values = new ArrayList<>(registryValues.size());
+    for (final V value : registryValues) {
+      if (condition.test(value)) {
+        values.add(value);
+      }
+    }
+    return (C) values;
+  }
+
+  /**
+   * {@inheritDoc}
+   * <p>
+   * This function only performs a call to {@link Map#clear()}.
+   *
+   * @since 2.3.0
+   */
+  @Override
+  public void unregisterAll() {
+    this.cache.clear();
+  }
+}
