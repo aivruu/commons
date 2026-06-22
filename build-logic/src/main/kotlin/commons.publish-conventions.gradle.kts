070258ecd87596ce8d91c0115fe608add936a9d8
aivruu <benjaalejandro206@gmail.com>
2026-06-22 01:00:21 -0400

    fix: core subproject not being resolvable due to not-found dependency for shaded fork

--- build-logic/src/main/kotlin/commons.publish-conventions.gradle.kts
+++ build-logic/src/main/kotlin/commons.publish-conventions.gradle.kts
@@ -1,3 +1,5 @@
+import groovy.util.Node
+
 plugins {
   id("commons.common-conventions")
   `maven-publish`
@@ -7,8 +9,22 @@ publishing {
   publications {
     create<MavenPublication>("maven") {
       artifactId = artifactId.substring(rootProject.name.length + 1)
-
       from(components["java"])
+
+      // exclude custom-fork artifacts
+      if (artifactId.equals("core")) {
+        pom {
+          withXml {
+            asNode().children().filterIsInstance<Node>()
+              .find { it.name() == "dependencies" }
+              ?.children()?.removeIf { dep ->
+                (dep as? Node)?.let {
+                  (it.get("artifactId") as? Node)?.text() == "configurate"
+                } ?: false
+              }
+          }
+        }
+      }
     }
   }
 }
