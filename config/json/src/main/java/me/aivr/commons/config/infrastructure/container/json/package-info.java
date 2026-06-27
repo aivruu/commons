fc796c5a2c32824f3f5ef72924551061eff73613
aivruu <benjaalejandro206@gmail.com>
2026-06-27 18:09:32 -0400

    docs: provide documentation for new configuration-related packages

---
+++ config/json/src/main/java/me/aivr/commons/config/infrastructure/container/json/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Provides access to infrastructure-layer JSON-support configuration-related components.
+ *
+ * @since 3.0.0
+ */
+@NullMarked
+package me.aivr.commons.config.infrastructure.container.json;
+
+import org.jspecify.annotations.NullMarked;
---
+++ config/src/main/java/me/aivr/commons/config/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides access to application and infrastructure-layer configuration-related components.
+ *
+ * @since 3.0.0
+ */
+package me.aivr.commons.config;
