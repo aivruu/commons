4f11b07d47c967a0b1228488c33769e8588a0d3f
aivruu <benjaalejandro206@gmail.com>
2026-06-27 18:07:45 -0400

    docs: move configuration-related package-documentation to dedicated config-subprojects

---
+++ config/src/main/java/me/aivr/commons/config/application/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Provides application configuration-related classes for structuration.
+ *
+ * @since 1.0.0
+ */
+@NullMarked
+package me.aivr.commons.config.application;
+
+import org.jspecify.annotations.NullMarked;
