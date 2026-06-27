3f4cd35365e135293f48054de8c84d3062177475
aivruu <benjaalejandro206@gmail.com>
2026-06-27 18:05:57 -0400

    build(config-conventions): write conventions-plugin for configuration-related subprojects

---
+++ build-logic/src/main/kotlin/commons.config-conventions.gradle.kts
@@ -0,0 +1,34 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar.Companion.shadowJar
+import org.gradle.accessors.dm.LibrariesForLibs
+import org.gradle.kotlin.dsl.getByType
+
+plugins {
+  id("commons.shadow-conventions")
+  id("commons.test-conventions")
+}
+
+repositories {
+  flatDir {
+    dirs("$projectDir/libs") // import local libs
+  }
+}
+
+val libs = extensions.getByType(LibrariesForLibs::class)
+
+dependencies {
+  val forkVersion: String = libs.versions.configurate.fork.get()
+  // Assuming is available in the 'libs' dir of each subproject of 'config'.
+  implementation(":configurate-core-$forkVersion")
+  testImplementation(":configurate-core-$forkVersion")
+
+  // Required by configurate-core
+  compileOnly(libs.geantyref)
+  testImplementation(libs.geantyref)
+}
+
+tasks {
+  shadowJar {
+    val route = "${findProperty("group") as String}.library.infrastructure.org.spongepowered.configurate"
+    relocate("org.spongepowered.configurate", route)
+  }
+}
