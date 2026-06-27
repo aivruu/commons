6a8fcd1fc4fbe3ad975d46c821f5395b05d19818
aivruu <benjaalejandro206@gmail.com>
2026-06-27 16:49:05 -0400

    build(core): remove code for configuration-libs inclusion

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,29 +5,4 @@ plugins {
 
 dependencies {
   api(project(":${rootProject.name}-api"))
-
-  // As we're using a fork, we need to exclude the original core-module.
-  compileOnlyApi(libs.configurate) {
-    exclude(group = "org.spongepowered", module = "configurate-core")
-  }
-  includeLocalLibs()
-}
-
-fun DependencyHandlerScope.includeLocalLibs() {
-  val forkVersion = "4.3.1-SNAPSHOT"
-
-  listOf("configurate-core", "configurate-gson", "configurate-yaml").forEach {
-    compileOnly(":$it-$forkVersion")
-    testImplementation(":$it-$forkVersion")
-  }
-
-  // Required by configurate-core
-  compileOnly(libs.geantyref)
-  testRuntimeOnly(libs.geantyref)
-}
-
-repositories {
-  flatDir {
-    dirs("libs") // import local libs
-  }
 }
