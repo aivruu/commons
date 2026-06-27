596a722dc45ba1e945d5c2141a93652ef15a6f4e
aivruu <benjaalejandro206@gmail.com>
2026-06-27 18:00:59 -0400

    feat(config)!: modify configuration-related structure.
    BREAKING CHANGE:
    Move configuration-related components each one to a dedicated subproject and remove originals from `core` subproject, including packages-documentation
    
    Remove generics for configuration-related builders except for class-type specification.
    
    Improve unit-tests for configuration-components.

---
+++ config/yaml/src/main/java/me/aivr/commons/config/infrastructure/container/yaml/YamlConfigurationContainer.java
@@ -0,0 +1,89 @@
+// This file is part of "commons", licensed under the GNU License.
+//
+// Copyright (c) 2026 aivruu
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+package me.aivr.commons.config.infrastructure.container.yaml;
+
+import me.aivr.commons.config.application.Configuration;
+import me.aivr.commons.config.application.Container;
+import org.jspecify.annotations.Nullable;
+import org.spongepowered.configurate.CommentedConfigurationNode;
+import org.spongepowered.configurate.ConfigurateException;
+import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
+
+/**
+ * A mutable-container for single YAML-related configuration-data storage.
+ *
+ * @param <Config> an object that extends from the {@link Configuration} interface.
+ * @since 3.0.0
+ */
+public final class YamlConfigurationContainer<Config extends Configuration> implements Container<Config> {
+  private final @Nullable String header;
+  private final Class<? extends Configuration> clazzType;
+  private final YamlConfigurationLoader loader;
+  private Config model;
+
+  /**
+   * Creates a new {@link YamlConfigurationContainer} with the provided parameters.
+   *
+   * @param header the header for the config's file, {@code null} if none.
+   * @param clazzType the class-type of the config-model.
+   * @param loader the configuration loader.
+   * @param model the actual configuration-model already loaded.
+   * @since 3.0.0
+   */
+  public YamlConfigurationContainer(
+      final @Nullable String header,
+      final Class<? extends Configuration> clazzType,
+      final YamlConfigurationLoader loader,
+      final Config model) {
+    this.header = header;
+    this.clazzType = clazzType;
+    this.loader = loader;
+    this.model = model;
+  }
+
+  @Override
+  public Config model() {
+    return this.model;
+  }
+
+  @Override
+  public @Nullable String header() {
+    return this.header;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public Class<Config> modelClass() {
+    return (Class<Config>) this.clazzType;
+  }
+
+  @Override
+  @SuppressWarnings({"unchecked", "ConstantConditions"}) // ignore unchecked-cast, newConfig won't be null
+  public boolean reload() {
+    try {
+      final CommentedConfigurationNode node = this.loader.load();
+      final Config newConfig = (Config) node.get(this.clazzType);
+      if (newConfig == this.model) {
+        return false;
+      }
+      this.model = newConfig;
+      return true;
+    } catch (final ConfigurateException exception) {
+      throw new RuntimeException(exception);
+    }
+  }
+}
--- config/yaml/src/main/java/me/aivr/commons/config/infrastructure/container/yaml/YamlContainerBuilder.java
+++ config/yaml/src/main/java/me/aivr/commons/config/infrastructure/container/yaml/YamlContainerBuilder.java
@@ -14,37 +14,37 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
-package me.aivr.commons.config.infrastructure.container.type;
+package me.aivr.commons.config.infrastructure.container.yaml;
 
 import com.google.common.base.Preconditions;
 import me.aivr.commons.config.application.Configuration;
 import me.aivr.commons.config.application.Container;
 import me.aivr.commons.config.infrastructure.ConfigType;
 import me.aivr.commons.config.infrastructure.container.ContainerBuilder;
-import me.aivr.commons.config.infrastructure.container.YamlConfigurationContainer;
 import org.jspecify.annotations.Nullable;
 import org.spongepowered.configurate.CommentedConfigurationNode;
 import org.spongepowered.configurate.ConfigurateException;
+import org.spongepowered.configurate.ConfigurationOptions;
 import org.spongepowered.configurate.yaml.NodeStyle;
 import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.function.UnaryOperator;
 
 /**
  * Represents a {@link ContainerBuilder} type for {@link YamlConfigurationContainer}s.
  *
- * @param <Config> a class that extends from {@link Configuration}.
- * @since 2.4.0
+ * @since 3.0.0
  */
-public final class YamlContainerBuilder<Config extends Configuration> extends ContainerBuilder<Config> {
+public final class YamlContainerBuilder extends ContainerBuilder {
   private @Nullable String header;
 
   /**
    * Creates a new {@link YamlContainerBuilder} from the provided parameter.
    *
    * @param directory the directory to use to resolve the YAML-file.
-   * @since 2.4.0
+   * @since 3.0.0
    */
   public YamlContainerBuilder(final Path directory) {
     super(directory);
@@ -54,12 +54,11 @@ public final class YamlContainerBuilder<Config extends Configuration> extends Co
    * Creates a new {@link YamlContainerBuilder} from the provided {@code directory} parameter.
    *
    * @param directory the directory where store the configuration-file.
-   * @param <Config> a class that extends from {@link Configuration}.
    * @return a new {@link YamlContainerBuilder}.
-   * @since 2.4.0
+   * @since 3.0.0
    */
-  public static <Config extends Configuration> YamlContainerBuilder<Config> create(final Path directory) {
-    return new YamlContainerBuilder<>(directory);
+  public static YamlContainerBuilder create(final Path directory) {
+    return new YamlContainerBuilder(directory);
   }
 
   /**
@@ -67,34 +66,50 @@ public final class YamlContainerBuilder<Config extends Configuration> extends Co
    *
    * @param header the header for the config-file.
    * @return this builder.
-   * @since 2.4.0
+   * @since 3.0.0
    */
-  public YamlContainerBuilder<Config> header(final @Nullable String header) {
+  public YamlContainerBuilder header(final @Nullable String header) {
     this.header = header;
     return this;
   }
 
   @Override
-  public Container<Config> build() {
+  protected UnaryOperator<ConfigurationOptions> resolveOptions() {
+    return opts -> {
+      if (super.configOptions == null) {
+        return Container.BUILT_IN_OPTIONS.apply(opts, this.header);
+      }
+      ConfigurationOptions resolvedProvidedOptions = super.configOptions.apply(opts);
+      // Apply header if it wasn't configured in the custom-options for the file.
+      if (this.header != null && resolvedProvidedOptions.header() == null) {
+        resolvedProvidedOptions = resolvedProvidedOptions.header(this.header);
+      }
+      return resolvedProvidedOptions;
+    };
+  }
+
+  @Override
+  @SuppressWarnings({"unchecked", "ConstantConditions"}) // ignore unchecked-cast, suppress node#get null-warning
+  public <C extends Configuration> Container<C> build() {
     Preconditions.checkNotNull(super.configClass, "Unspecified config class-type for YAML-container construction.");
-    Preconditions.checkNotNull(super.fileName, "Unspecified file-name for YAML-configuration construction.");
-    Preconditions.checkArgument(!super.fileName.isEmpty(), "Empty file-name for YAML-configuration construction.");
+    Preconditions.checkArgument(super.fileName != null && !super.fileName.isEmpty(),
+        "Unspecified or empty file-name for YAML-configuration construction.");
 
     final Path path = super.directory.resolve(super.fileName + ConfigType.YAML.fileExtension());
     final YamlConfigurationLoader loader = YamlConfigurationLoader.builder()
         .nodeStyle(NodeStyle.BLOCK)
         .commentsEnabled(true) // finally
-        .defaultOptions(super.configOptions == null ? opts -> Container.BUILT_IN_OPTIONS.apply(opts, this.header) : super.configOptions)
+        .defaultOptions(this.resolveOptions())
         .path(path)
         .build();
     try {
       final CommentedConfigurationNode node = loader.load();
-      final Config config = node.get(super.configClass);
+      final C config = (C) node.get(super.configClass);
       if (Files.notExists(path)) {
         node.set(super.configClass, config);
         loader.save(node);
       }
-      return new YamlConfigurationContainer<>(config, this.header, loader, super.configClass);
+      return new YamlConfigurationContainer<>(this.header, super.configClass, loader, config);
     } catch (final ConfigurateException exception) {
       throw new RuntimeException(exception);
     }
