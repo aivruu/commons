4f0f670688637fcb3e65d9e2ce5d7902c8484a5d
aivruu <benjaalejandro206@gmail.com>
2026-06-27 18:03:31 -0400

    feat(settings.gradle.kts): improve subproject-configuration and include configuration-related ones

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,14 +10,19 @@ plugins {
 
 rootProject.name = "commons"
 
-sequenceOf("api", "core").forEach {
-    val kerbalProject = "${rootProject.name}-$it"
-    include(kerbalProject)
-    project(":$kerbalProject").projectDir = file(it)
-}
+listOf("api", "core").forEach { importProject(it, null) }
 
-sequenceOf("json", "mongo", "local").forEach {
-    val kerbalProject = "${rootProject.name}-infrastructure-$it"
-    include(kerbalProject)
-    project(":$kerbalProject").projectDir = file("infrastructure/$it")
+// config-related subprojects
+importProject("config", null)
+listOf("yaml", "json", "toml").forEach { importProject(it, "config") }
+
+// infrastructure-related
+listOf("json", "mongo", "local").forEach { importProject(it, "infrastructure") }
+
+fun importProject(name: String, dir: String?) {
+  val inSubDirectory = dir != null
+  val validRoute = if (inSubDirectory) "$dir/$name" else name
+  val kerbalProject = "${rootProject.name}${if (inSubDirectory) "-$dir" else ""}-$name"
+  include(kerbalProject)
+  project(":$kerbalProject").projectDir = file(validRoute)
 }
