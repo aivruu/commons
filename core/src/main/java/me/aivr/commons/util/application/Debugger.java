afa1a31fe23ad3f93412f822e819b1d6c6201c33
aivruu <benjaalejandro206@gmail.com>
2026-06-27 18:05:38 -0400

    feat(application/Debugger): improve utility with more features.
    Allow to use Logger#debug function if required, needs to be configured.
    
    Allow to use a custom-name logger instance.
    
    Provide functions to know whether the debugger is enabled or if it's using `DEBUG` level logging

--- core/src/main/java/me/aivr/commons/util/application/Debugger.java
+++ core/src/main/java/me/aivr/commons/util/application/Debugger.java
@@ -16,6 +16,7 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 package me.aivr.commons.util.application;
 
+import org.jspecify.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,20 +27,46 @@ import org.slf4j.LoggerFactory;
  */
 public final class Debugger {
   /**
-   * The logger-instance to use for the messages.
+   * The logger-instance to use for the messages, {@code null} if not configured yet.
    *
-   * @since 1.0.0
+   * @since 3.0.0
    */
-  private static final Logger LOGGER = LoggerFactory.getLogger(Debugger.class);
+  private static @Nullable Logger internalLogger;
   /**
    * Indicates whether the debugger is enabled or not.
    *
    * @since 1.0.0
    */
   private static boolean enabled;
+  /**
+   * Indicates whether the debugger should use {@link Logger#debug(String, Object...)} rather that {@link Logger#info(String, Object...)}.
+   *
+   * @since 3.0.0
+   */
+  private static boolean useDebugFunction;
 
   private Debugger() {
     throw new UnsupportedOperationException("This class is for utility and cannot be instantiated.");
+  }
+
+  /**
+   * Creates a new {@link Logger} instance with the name provided for this function.
+   *
+   * @param customName the name the logger-instance will have.
+   * @since 3.0.0
+   */
+  public static void initLogger(final String customName) {
+    if (internalLogger == null) internalLogger = LoggerFactory.getLogger(customName);
+  }
+
+  /**
+   * Checks whether the debugger is or not enabled.
+   *
+   * @return {@code true} if it's enabled, {@code false} otherwise.
+   * @since 3.0.0
+   */
+  public static boolean enabled() {
+    return enabled;
   }
 
   /**
@@ -53,13 +80,53 @@ public final class Debugger {
   }
 
   /**
+   * Checks whether for logging the debugger must use the {@link Logger#debug(String, Object...)} function.
+   *
+   * @return {@code true} if it should use it, {@code false} otherwise.
+   * @since 3.0.0
+   */
+  public static boolean usingDebugFunction() {
+    return useDebugFunction;
+  }
+
+  /**
+   * Sets the {@link #useDebugFunction} state for the debugger to use debug-specific {@link Logger} functions.
+   * <p>
+   * Using this logger-level requires to have a binding configured (e.g. logback) in order to use that, otherwise, it can be
+   * configured specifying the property for the JVM -> {@code -Dorg.slf4j.simpleLogger.defaultLogLevel=debug}.
+   *
+   * @param should whether allow the debugger to use the {@link Logger#debug(String, Object...)} function.
+   * @since 3.0.0
+   */
+  public static void useDebugFunction(final boolean should) {
+    useDebugFunction = should;
+  }
+
+  /**
    * Logs the given debug-message and will apply the given args as "placeholders", if the debugger is enabled.
+   * <p>
+   * If {@link #internalLogger} hasn't been initialized yet, this function will initialize it calling to the
+   * {@link #initLogger(String)} function and giving this class's name as parameter.
+   * <p>
+   * If {@link #useDebugFunction} is configured to {@code true}, the debugger will use the {@link Logger#debug(String, Object...)}
+   * instead.
    *
    * @param message the message to log.
    * @param args the values to replace in the message.
-   * @since 1.0.0
+   * @see Class#getSimpleName() Get class's name for the Logger
+   * @since 3.0.0
    */
   public static void write(final String message, final Object... args) {
-    if (enabled) LOGGER.debug(message, args);
+    if (!enabled) return;
+
+    if (internalLogger == null) {
+      initLogger(Debugger.class.getSimpleName());
+    }
+    // if logger is configured...
+    if (usingDebugFunction()) {
+      internalLogger.debug(message, args);
+      return;
+    }
+    internalLogger.info(message, args);
   }
 }
