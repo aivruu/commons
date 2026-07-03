1d144dbc3dda2b9011768c397fe99066b237d086
aivruu <benjaalejandro206@gmail.com>
2026-07-02 20:50:08 -0400

    build(settings): provide function for single-dir subprojects inclusion, and include new subprojects for the project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,25 @@
 @file:Suppress("UnstableApiUsage")
 
 pluginManagement {
-    includeBuild("build-logic")
+  includeBuild("build-logic")
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+  id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "commons"
+// single modules import.
+listOf("core", "event", "parser").forEach { importProject(it) }
 
-listOf("api", "core").forEach { importProject(it, null) }
-
-// config-related subprojects
-importProject("config", null)
+importProject("config")
 listOf("yaml", "json", "toml").forEach { importProject(it, "config") }
 
-// infrastructure-related
 listOf("json", "mongo", "local").forEach { importProject(it, "infrastructure") }
+
+fun importProject(name: String) {
+  importProject(name, null)
+}
 
 fun importProject(name: String, dir: String?) {
   val inSubDirectory = dir != null
