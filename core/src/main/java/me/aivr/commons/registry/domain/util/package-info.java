4bf1aff8349610a183bf80f59e1fed9a838a5f9e
aivruu <benjaalejandro206@gmail.com>
2026-07-04 21:45:54 -0400

    docs: provide documentation for `domain.util` package related to registries.

---
+++ core/src/main/java/me/aivr/commons/registry/domain/util/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Provides registry-related domain utilities.
+ *
+ * @since 3.0.0-rc2
+ */
+@NullMarked
+package me.aivr.commons.registry.domain.util;
+
+import org.jspecify.annotations.NullMarked;
