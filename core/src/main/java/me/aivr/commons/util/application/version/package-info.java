13e8b91ef70b31a37d236b5199af70b3f015d962
aivruu <benjaalejandro206@gmail.com>
2026-08-11 18:59:11 -0400

    docs(application/version): provide documentation for the `application/version` package

---
+++ core/src/main/java/me/aivr/commons/util/application/version/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Provides classes to detect and access version-specific information for the on-running minecraft-version.
+ *
+ * @since 3.2.0
+ */
+@NullMarked
+package me.aivr.commons.util.application.version;
+
+import org.jspecify.annotations.NullMarked;
